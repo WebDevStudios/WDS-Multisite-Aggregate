686aa59a07a9b25d1a26e3fc7fa33745486070c1
Justin Sternberg <justin@dsgnwrks.pro>
2014-06-26 15:25:22 -0400

    Move options saving to options class

--- wds-multisite-aggregate.php
+++ wds-multisite-aggregate.php
@@ -46,174 +46,30 @@ spl_autoload_register( 'wds_ma_autoload_classes' );
 class WDS_Multisite_Aggregate {
 
 	public function __construct() {
+		// Handles Admin display
 		$this->admin = new WDS_Multisite_Aggregate_Admin();
 		$this->admin->hooks();
+		// Options setter/getter and handles updating options on save
 		$this->options = new WDS_Multisite_Aggregate_Options();
+		$this->options->hooks();
+		// Handles removing posts from removed blogs
+		$this->remove = new WDS_Multisite_Aggregate_Remove( $this->options );
+		$this->remove->hooks();
+		// Handles frontend modification for aggregate site
+		$this->frontend = new WDS_Multisite_Aggregate_Frontend( $this->options );
+		$this->frontend->hooks();
 	}
 
 	function hooks() {
-		add_action('save_post', array( $this, 'do_post_sync' ), 10, 2);
+		add_action( 'save_post', array( $this, 'do_post_sync' ), 10, 2 );
 		add_action( 'trash_post', array( $this, 'sync_post_delete' ) );
 		add_action( 'delete_post', array( $this, 'sync_post_delete' ) );
-
-		if ( !empty( $_GET[ 'page' ] ) && $_GET[ 'page' ] == 'wds-multisite-aggregate' ) {
-			add_action( 'admin_init', array( $this, 'update_options' ) );
-		}
 
 		if ( !empty( $_GET[ 'action' ] ) && $_GET[ 'action' ] == 'sitewidetags-populate' ) {
 			add_action( 'init', array( $this, 'populate_posts' ) );
 		}
 
-		/* complete blog actions ($blog_id != 0) */
-		add_action('delete_blog', array( $this, 'remove_sync_posts' ), 10, 1);
-		add_action('archive_blog', array( $this, 'remove_sync_posts' ), 10, 1);
-		add_action('deactivate_blog', array( $this, 'remove_sync_posts' ), 10, 1);
-		add_action('make_spam_blog', array( $this, 'remove_sync_posts' ), 10, 1);
-		add_action('mature_blog', array( $this, 'remove_sync_posts' ), 10, 1);
-		/* single post actions ($blog_id == 0) */
-		add_action("transition_post_status", array( $this, 'remove_sync_posts' ));
-
-		add_action('update_option_blog_public', array( $this, 'public_blog_update' ), 10, 2);
-
-		add_filter( 'post_link', array( $this, 'post_link' ), 10, 2 );
-		add_filter( 'page_link', array( $this, 'post_link' ), 10, 2 );
-
 		add_filter( 'sitewide_tags_allowed_post_types', array( $this, 'pages_filter' ) );
-		add_filter('post_thumbnail_html', array( $this, 'thumbnail_link' ), 10, 2);
-	}
-
-	function update_options() {
-		global $wpdb, $current_site, $current_user, $wp_version;
-
-		$valid_nonce = isset($_REQUEST['_wpnonce']) ? wp_verify_nonce($_REQUEST['_wpnonce'], 'wds-multisite-aggregate') : false;
-		if ( !$valid_nonce )
-			return false;
-
-		if ( $_GET[ 'action' ] == 'populateblogs' ) {
-			$id = isset( $_GET[ 'populate_blog' ] ) ? (int)$_GET[ 'populate_blog' ] : 0;
-			$c = isset( $_GET[ 'c' ] ) ? (int)$_GET[ 'c' ] : 0; // blog count
-			$p = isset( $_GET[ 'p' ] ) ? (int)$_GET[ 'p' ] : 0; // post count
-			$all = isset( $_GET[ 'all' ] ) ? (int)$_GET[ 'all' ] : 0; // all blogs
-
-			if  ( $id == 0 && isset( $_GET[ 'populate_all_blogs' ] ) ) // all blogs.
-				$all = 1;
-
-			$tags_blog_id = $this->options->get( 'tags_blog_id' );
-			if( !$tags_blog_id )
-				return false;
-
-			if( $all )
-				$blogs = $wpdb->get_col( $wpdb->prepare( "SELECT blog_id FROM $wpdb->blogs ORDER BY blog_id DESC LIMIT %d,5", $c ) );
-			else
-				$blogs = array( $id );
-
-			foreach( $blogs as $blog ) {
-				if( $blog != $tags_blog_id ) {
-					$details = get_blog_details( $blog );
-					$url = add_query_arg( array( 'p' => $p, 'action' => 'sitewidetags-populate', 'key' => md5( serialize( $details ) ) ), $details->siteurl );
-					$p = 0;
-					$post_count = 0;
-
-					$result = wp_remote_get( $url );
-					if( isset( $result['body'] ) )
-						$post_count = (int)$result['body'];
-
-					if( $post_count ) {
-						$p = $post_count;
-						break;
-					}
-				}
-				$c++;
-			}
-			if( !empty( $blogs ) && ( $all || $p ) ) {
-				if ( version_compare( $wp_version, '3.0.9', '<=' ) && version_compare( $wp_version, '3.0', '>=' ) )
-					$url = admin_url( 'ms-admin.php' );
-				else
-					$url = network_admin_url( 'settings.php' );
-
-				wp_redirect( wp_nonce_url( $url , 'wds-multisite-aggregate' ) . "&page=sitewidetags&action=populateblogs&c=$c&p=$p&all=$all" );
-				die();
-			}
-			wp_die( 'Finished importing posts into tags blogs!' );
-		}
-
-		if( !$_POST[ 'tags_blog_enabled' ] ) {
-			if( $this->options->get( 'tags_blog_enabled' ) != $_POST[ 'tags_blog_enabled' ] )
-				$this->options->get( 'tags_blog_enabled', 0, true );
-			wp_redirect( add_query_arg( array( 'updated' => '1' ) ) );
-			exit;
-		}
-		$this->options->get( 'tags_blog_enabled', 1 );
-
-		if( ( isset( $_POST[ 'tags_blog' ] ) || isset( $_POST[ 'tags_blog_main_blog' ] ) ) && isset( $_POST[ 'tags_blog_public' ] ) ) {
-			if( $_POST[ 'tags_blog_main_blog' ] == 1 ) {
-				if( $current_site->blog_id )
-					$id = $current_site->blog_id;
-				else
-					$id = $wpdb->get_var( "SELECT blog_id FROM {$wpdb->blogs} WHERE domain = '{$current_site->domain}' AND path = '{$current_site->path}'" );
-				if( $id ) {
-					$this->options->get( 'tags_blog_id', $id );
-					$this->options->get( 'tags_blog_main_blog', 1 );
-				} else {
-					$this->options->get( 'tags_blog_main_blog', 0 );
-				}
-			} else {
-				$this->options->get( 'tags_blog_main_blog', 0 );
-				$tags_blog = $_POST[ 'tags_blog' ];
-				$this->options->get( 'tags_blog', $tags_blog );
-				if( constant( 'VHOST' ) == 'yes' ) {
-					$domain = $tags_blog . '.' . $current_site->domain;
-					$path = $current_site->path;
-				} else {
-					$domain = $current_site->domain;
-					$path = trailingslashit( $current_site->path . $tags_blog );
-				}
-				$tags_blog_id = $wpdb->get_var( "SELECT blog_id FROM {$wpdb->blogs} WHERE domain = '$domain' AND path = '$path'" );
-				if( $tags_blog_id ) {
-					update_sitewide_tags_option( 'tags_blog_id', $tags_blog_id );
-				} else {
-					$wpdb->hide_errors();
-					$id = wpmu_create_blog( $domain, $path, __( 'Global Posts','wds-multisite-aggregate' ), $current_user->id , array( "public" => $_POST[ 'tags_blog_public' ] ), $current_site->id);
-					update_sitewide_tags_option( 'tags_blog_id', $id );
-					$wpdb->show_errors();
-				}
-			}
-			$tags_blog_public = (int)$_POST[ 'tags_blog_public' ];
-			update_sitewide_tags_option( 'tags_blog_public', $tags_blog_public );
-			update_blog_option( $tags_blog_id, 'blog_public', $tags_blog_public );
-			update_blog_status( $tags_blog_id, 'public', $tags_blog_public);
-		}
-
-		if( isset( $_POST[ 'tags_max_posts' ] ) )
-			update_sitewide_tags_option( 'tags_max_posts', (int)$_POST[ 'tags_max_posts' ] );
-
-		if( $this->options->get( 'tags_blog_pages' ) != $_POST[ 'tags_blog_pages' ] )
-			update_sitewide_tags_option( 'tags_blog_pages', (int)$_POST[ 'tags_blog_pages' ] );
-
-		if( $this->options->get( 'tags_blog_thumbs' ) != $_POST[ 'tags_blog_thumbs' ] )
-			update_sitewide_tags_option( 'tags_blog_thumbs', (int)$_POST[ 'tags_blog_thumbs' ] );
-
-		if( $this->options->get( 'tags_blog_pub_check' ) != $_POST[ 'tags_blog_pub_check' ] ) {
-			if( $tags_blog_public == 0 )
-				update_sitewide_tags_option( 'tags_blog_pub_check', (int)$_POST[ 'tags_blog_pub_check' ] );
-			else
-				update_sitewide_tags_option( 'tags_blog_pub_check', 0 );
-		}
-
-		if( $_POST['tags_blog_postmeta'] != '' ) {
-			$meta_keys = split( "\n", stripslashes( $_POST[ 'tags_blog_postmeta' ] ) );
-			foreach( (array) $meta_keys as $key ) {
-				$keys[] = trim( $key );
-			}
-			update_sitewide_tags_option( "tags_blog_postmeta", $keys );
-		} else {
-			update_sitewide_tags_option( "tags_blog_postmeta", '' );
-		}
-
-		// force write if changes saved
-		update_sitewide_tags_option( true );
-		wp_redirect( add_query_arg( array( 'updated' => '1' ) ) );
-		exit;
 	}
 
 	/**
@@ -223,14 +79,16 @@ class WDS_Multisite_Aggregate {
 		global $wpdb;
 
 		$valid_key = isset( $_REQUEST['key'] ) ? $_REQUEST['key'] == md5( serialize( get_blog_details( $wpdb->blogid ) ) ) : false;
-		if ( !$valid_key )
+		if ( !$valid_key ) {
 			return false;
+		}
 
 		$tags_blog_id = $this->options->get( 'tags_blog_id' );
 		$tags_blog_enabled = $this->options->get( 'tags_blog_enabled' );
 
-		if( !$tags_blog_enabled || !$tags_blog_id || $tags_blog_id == $wpdb->blogid )
+		if ( !$tags_blog_enabled || !$tags_blog_id || $tags_blog_id == $wpdb->blogid ) {
 			exit( '0' );
+		}
 
 		$posts_done = 0;
 		$p = isset( $_GET[ 'p' ] ) ? (int)$_GET[ 'p' ] : 0; // post count
@@ -241,8 +99,9 @@ class WDS_Multisite_Aggregate {
 				exit( '0' );
 
 			foreach ( $posts as $post ) {
-				if ( $post != 1 && $post != 2 )
+				if ( $post != 1 && $post != 2 ) {
 					$this->do_post_sync( $post, get_post( $post ) );
+				}
 
 			}
 			$posts_done += 50;
@@ -385,139 +244,14 @@ class WDS_Multisite_Aggregate {
 		restore_current_blog();
 	}
 
-	/**
-	 * remove all posts from a given blog ($blog_id != 0)
-	 * - used if a blog is deleted or marked as deactivat, spam, archive, mature
-	 * - also runs if a blog is switched to a none public blog (called by
-	 *   public_blog_update), more details on public_blog_update
-	 * removes some posts if the limit is reached ($blog_id == 0)
-	 * - triggered by other actions but without an given blog_id
-	 * - number of posts to delete in $max_to_del
-	 *
-	 * @param $blog_id
-	 */
-	function remove_sync_posts($blog_id = 0) {
-		global $wpdb;
-		$tags_blog_id = $this->options->get( 'tags_blog_id' );
-		$max_to_del = 10;
-
-		if( !$tags_blog_id )
-			return;
-
-		/* actions on the tags blog */
-		if ( ($blog_id == 0) && ($wpdb->blogid == $tags_blog_id) )
-			return;
-		if ( $tags_blog_id == $blog_id )
-			return;
-
-		switch_to_blog( $tags_blog_id );
-
-		if ( $blog_id != 0 ) {
-			$posts = $wpdb->get_col( "SELECT ID FROM {$wpdb->posts} WHERE guid LIKE '" . $blog_id . ".%' OR guid LIKE '" . esc_url( $blog_id ) . ".%'" );
-			if( is_array( $posts ) && !empty( $posts ) ) {
-				foreach( $posts as $p_id ) {
-					wp_delete_post( $p_id );
-				}
-			}
-		} else {
-			/* delete all posts over the max limit */
-			if( mt_rand( 0, 10 ) ) {
-				$allowed_post_types = apply_filters( 'sitewide_tags_allowed_post_types', array( 'post' => true ) );
-				if( is_array( $allowed_post_types ) && !empty( $allowed_post_types ) ) {
-					$post_types = array();
-					foreach( $allowed_post_types as $k => $v ) {
-						if( $v ) {
-							$post_types[] = $k;
-						}
-					}
-					if( is_array( $post_types ) && !empty( $post_types ) ) {
-						if( count( $post_types ) > 1 )
-							$where = "IN ('" . join( "','", $post_types ) . "') ";
-						else
-							$where = "= '" . $post_types[0] . "' ";
-					} else {
-						$where = "= 'post' ";
-					}
-					$posts = $wpdb->get_results( "SELECT ID, guid FROM {$wpdb->posts} WHERE post_status='publish' AND post_type {$where} ORDER BY ID DESC limit " . $this->options->get( 'tags_max_posts', 5000 ) . ", " . $max_to_del );
-					if( is_array( $posts ) && !empty( $posts ) ) {
-						foreach( $posts as $p ) {
-							if( preg_match('|^.*\.([0-9]+)$|', $p->guid, $matches) && intval( $matches[1] ) > 0 )
-								wp_delete_post( $p->ID );
-						}
-					}
-				}
-			}
-		}
-		restore_current_blog();
-	}
-
-	/**
-	 * called as an action if the public state for a blog is switched
-	 * - if a blog becomes not public - all posts in the tags blog will be removed
-	 * - bug on 1.5.1: update_option_blog_public is only triggered if the public state
-	 *   is changed from the backend - from edit blog as siteadmin the action isn't
-	 *   running and the state in the blogs backend isn't changed
-	 *
-	 * @param int $old - old public state
-	 * @param int $new - new state, public == 1, not public == 0
-	 */
-	function public_blog_update($old, $new) {
-		global $wpdb;
-		$tags_blog_id = $this->options->get( 'tags_blog_id' );
-
-		if( !$tags_blog_id )
-			return;
-
-		/* the tags blog */
-		if ( $tags_blog_id == $wpdb->blogid )
-			return;
-
-		if ($new == 0 ) {
-			$this->remove_sync_posts($wpdb->blogid);
-		}
-	}
-
-	function post_link( $link, $post ) {
-		global $wpdb;
-		$tags_blog_id = $this->options->get( 'tags_blog_id' );
-		if( !$tags_blog_id )
-			return $link;
-
-		if( $wpdb->blogid == $tags_blog_id ) {
-			if( is_numeric( $post ) )
-				$url = get_post_meta( $post, 'permalink', true );
-			else
-				$url = get_post_meta( $post->ID, "permalink", true );
-
-			if( $url )
-				return $url;
-		}
-
-		return $link;
-	}
-
 	function pages_filter( $post_types ) {
-		if( $this->options->get( 'tags_blog_pages' ) )
+		if ( $this->options->get( 'tags_blog_pages' ) ) {
 			$post_types = array_merge( $post_types, array( 'page' => true ) );
-		return $post_types;
-	}
-
-	function thumbnail_link( $html, $post_id ) {
-		global $wpdb;
-		$tags_blog_id = $this->options->get( 'tags_blog_id' );
-		if( !$tags_blog_id )
-			return $html;
-
-		if( $wpdb->blogid == $tags_blog_id ) {
-			$thumb = get_post_meta( $post_id, 'thumbnail_html', true );
-			if( $thumb )
-				return $thumb;
 		}
-		return $html;
+		return $post_types;
 	}
 
 }
 
 $WDS_Multisite_Aggregate = new WDS_Multisite_Aggregate();
 $WDS_Multisite_Aggregate->hooks();
-
